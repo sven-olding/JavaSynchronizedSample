5a15b5895c98fdc748cd6b92656d74409b7dbd81
Sven Olding <sven.olding@gmx.de>
2019-02-13 15:33:51 +0100

    initial commit

---
+++ src/SynchronizedMethodsTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+public class SynchronizedMethodsTest {
+	@Test
+	public void givenMultiThread_whenNonSyncMethod() throws InterruptedException {
+	    ExecutorService service = Executors.newFixedThreadPool(3);
+	    SynchronizedMethods summation = new SynchronizedMethods();
+	 
+	    IntStream.range(0, 1000)
+	      .forEach(count -> service.submit(summation::calculate));
+	    service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+	 
+	    assertEquals(1000, summation.getSum());
+	}
+	
+	@Test
+	public void givenMultiThread_whenMethodSync() throws InterruptedException {
+	    ExecutorService service = Executors.newFixedThreadPool(3);
+	    SynchronizedMethods method = new SynchronizedMethods();
+	 
+	    IntStream.range(0, 1000)
+	      .forEach(count -> service.submit(method::synchronisedCalculate));
+	    service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+	 
+	    assertEquals(1000, method.getSum());
+	}
+	
+	
+	@Test
+	public void givenMultiThread_whenStaticSyncMethod() throws InterruptedException {
+	    ExecutorService service = Executors.newCachedThreadPool();
+	 
+	    IntStream.range(0, 1000)
+	      .forEach(count -> 
+	        service.submit(SynchronizedMethods::syncStaticCalculate));
+	    service.awaitTermination(100, TimeUnit.MILLISECONDS);
+	 
+	    assertEquals(1000, SynchronizedMethods.staticSum);
+	}
+}
